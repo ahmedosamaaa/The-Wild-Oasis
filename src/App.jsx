d26f14d2f9d8252f72fe89b2b1598a8123a52798
ahmed <ahmedosama12313@gmail.com>
2024-01-27 05:53:53 +0200

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
 import styled from "styled-components";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
+const StyledApp = styled.main`
+  /* background-color: orangered; */
+  padding: 20px;
 `;
 
 function App() {
   return (
-    <div>
-      <H1>The Wild Oasis</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button
+                variation="primary"
+                size="medium"
+                onClick={() => alert("Check in")}
+              >
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check out")}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="number of guests" />
+              <Input type="number" placeholder="number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
   );
 }
 
