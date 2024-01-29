452153e8a9cf31263bce6054f9ad3daac459a432
ahmed <ahmedosama12313@gmail.com>
2024-01-29 05:55:34 +0200

    finishing creating table for cabins, create cabin feature using react hook form and notifications for success and errors

---
+++ src/services/apiCabins.js
@@ -0,0 +1,34 @@
+import supabase from "./supabase";
+
+export async function getCapins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("cabins could not be loaded");
+  }
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("cabins could not be created");
+  }
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("cabins could not be deleted");
+  }
+  return data;
+}
